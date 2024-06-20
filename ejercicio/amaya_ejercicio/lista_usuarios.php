894d71a4369f4b878ef75f3385dcfea78b01c3ce
Amayagod14 <149508957+Amayagod14@users.noreply.github.com>
2024-06-20 08:17:33 -0500

    Update lista_usuarios.php

--- ejercicio/amaya_ejercicio/lista_usuarios.php
+++ ejercicio/amaya_ejercicio/lista_usuarios.php
@@ -10,25 +10,37 @@ if (!isset($_SESSION['user'])) {
 $usuarios = Usuario::obtenerTodos();
 ?>
 
-<table border="1">
-    <tr>
-        <th>Documento</th>
-        <th>Nombre</th>
-        <th>Fecha de Nacimiento</th>
-        <th>Foto</th>
-        <th>Acciones</th>
-    </tr>
-    <?php foreach ($usuarios as $usuario): ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .highlight {
+            background-color: yellow;
+        }
+    </style>
+</head>
+<body>
+    <table border="1">
         <tr>
-            <td><?php echo $usuario['documento']; ?></td>
-            <td><?php echo $usuario['nombre']; ?></td>
-            <td><?php echo $usuario['fecha_nac']; ?></td>
-            <td><?php echo $usuario['foto']; ?></td>
-            <td>
-                <a href="actualizar.php?documento=<?php echo $usuario['documento']; ?>">Actualizar</a>
-            </td>
+            <th>Documento</th>
+            <th class="highlight">Nombre</th>
+            <th>Fecha de Nacimiento</th>
+            <th>Foto</th>
+            <th>Acciones</th>
         </tr>
-    <?php endforeach; ?>
-</table>
-<br>
+        <?php foreach ($usuarios as $usuario): ?>
+            <tr>
+                <td><?php echo $usuario['documento']; ?></td>
+                <td class="highlight"><?php echo $usuario['nombre']; ?></td>
+                <td><?php echo $usuario['fecha_nac']; ?></td>
+                <td><?php echo $usuario['foto']; ?></td>
+                <td>
+                    <a href="actualizar.php?documento=<?php echo $usuario['documento']; ?>">Actualizar</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    <br>
 <a href="registro.php">Vamos a registrar a un nuevo usuario!!!</a>
+</body>
+</html>
